3e34eab443c30aef82b13b3a4ee2e68f89bef1b0
karignzlz <jes.nefilim@gmail.com>
2025-04-04 06:21:11 -0300

    editando comentarios

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let contarMeGusta = document.querySelectorAll(".like");
 //seleciona cada boton
 botonMeGusta.forEach(function(boton, index) { //index es la posicion del boton
     let count = 0;
-    boton.addEventListener("click", function() { //adigna evento al boton
+    boton.addEventListener("click", function() { //asigna evento al boton
     count++;
     //sincroniza el contador con el boton correspondiente
-    contarMeGusta[index].textContent = count + " like(s)"; //actuliza el contador
+    contarMeGusta[index].textContent = count + " like(s)"; //actualiza el contador
     });
 });
